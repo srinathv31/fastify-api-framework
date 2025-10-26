1a7c7c19a2e2dcf77fd14bc111f244d0f0674021
Srinath Venkatesh <srinathvb1@gmail.com>
2025-10-26 15:47:08 -0400

    initial order endpoint

--- index.ts
+++ index.ts
@@ -1,9 +1,33 @@
 import fastify from "fastify";
 
-const server = fastify();
+const server = fastify({
+  logger: true,
+});
 
 server.get("/ping", async () => {
   return "pong\n";
+});
+
+server.post("/order", async (request, reply) => {
+  const { id } = request.body as { id: string };
+
+  if (!id) {
+    return reply.status(400).send({ error: "ID is required" });
+  }
+
+  // call workflow service to create order
+  const response = await fetch("http://localhost:3000/api/order", {
+    method: "POST",
+    body: JSON.stringify({ id }),
+  });
+
+  if (!response.ok) {
+    return reply.status(500).send({ error: "Failed to create order" });
+  }
+
+  const data = await response.json();
+
+  reply.status(201).send(data);
 });
 
 server.listen({ port: 8080 }, (err, address) => {
